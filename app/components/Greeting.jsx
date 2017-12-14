f76df56073c89c943c5fdffa4561e88833b39eb4
Nazar <kryvyynazariy@gmail.com>
2017-12-14 10:28:49 +0200

    fix

--- app/components/Greeting.jsx
+++ app/components/Greeting.jsx
@@ -4,7 +4,7 @@ export default class Greeting extends React.Component{
   render() {
     return (
         <div>
-            <h1>Hello user</h1>
+            <h1 className="text-center">Hello user</h1>
         </div>
     );
   }
